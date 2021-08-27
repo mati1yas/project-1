41416013376e7cc46cc363fe05e4ef746f301fb9
mehari123 <88460961+mehari123@users.noreply.github.com>
2021-08-27 14:24:12 +0300

    Update trial.cpp

--- trial.cpp
+++ trial.cpp
@@ -2,15 +2,17 @@
 #include<iomanip>
 #include<stdlib.h>
 #include<string.h>
+#include<string>
+#include <cstdlib>
+#include <bits/stdc++.h>
 using namespace std;
 struct Date{
 int yy,mm,dd;
 };
 struct Address{
-int house_no;
+string city;
 int phone;
 };
-
 struct Admin{
 string password,user_name;
 
@@ -29,10 +31,9 @@ struct User{
 string name, user_id;
 int age;
 Address user_address;
-int num_issued_book=0;
-Book issued_book;
-Date issue_date,return_date;
-}user[100];
+int num_issued_book;
+Book issued_book[2];
+};
 
 void welcome();
 int main_menu();
@@ -46,11 +47,28 @@ void by_rating();
 void recent_books();
 void admin_option();
 void all_books();
-void sampleData();
+
+
+
 int main(){
 welcome();
-sampleData();
-  bool stop=false;
+
+book[0].book_id ="b100";
+book[0].title="c++";
+book[0].author="Melaku";
+book[0].shelf_no="A20";
+book[0].catagory="art";
+book[0].rate=4.5;
+book[0].isbn=123;
+book[0].read_freq=3;
+book[0].quantity=122;
+book[0].edition=2;
+book[0].date_of_pub={2000,07,25};
+book[1]={"b101","Luxury life","Mahi Tz","A10","Self help",5,122,2,{2015,07,25},0,0,false};
+//book[2]={"b102","Tsom ","Mastewal ","A20","art",4.5,8,4568,2,{2001,06,23}};
+//book[3]={"b103","Java","Matiyas ","A20","Programing",4,6,7892,2,{2010,04,10}};
+//book[4]={"b104","sql","Matiyas ","A20","Programing",4.5,1,45587,2,{2020,04,15}};
+bool stop=false;
 do{
             int a=main_menu();
     switch (a){
@@ -173,24 +191,347 @@ for(int i=0; i<8; i++){cout << char(205);}cout << char(186); /*9*/for(int i=0; i
 
 cout <<endl;
 
-void sampleData(){
-//sample books
+}
+for (int i=0;i<20;i++){cout <<setw(10)<<x[i];}
+}
 
-book[0]={"b100","c++","Mahri","A20","Programing",3,122,2,{2000,07,25},0,0,false};
-book[1]={"b101","Luxury life","Mahi Tz","A10","Self help",5,1224,2,{2015,07,25},0,0,false};
-book[2]={"b102","Tsom ","Mastewal ","A20","Programing",8,4568,2,{2001,06,23},0,0,false};
-book[3]={"b103","Java","Matiyas ","A20","Programing",6,7892,2,{2010,04,10},0,0,false};
-book[4]={"b104","sql","Matiyas ","A20","Programing",1,45587,2,{2020,04,15},0,0,false};
+//
 
-//sample user
 
-user[0].name="Lebamlak";
-user[0].user_id="u100";
-user[0].age=20;
-user[0].user_address.house_no=1812;
-user[0].user_address.phone = 1994;
+
+void search1 (char a){
+int Num_search[20],counter=0;
+switch (a){
+case 'a':{cin.ignore();
+system ("cls");
+cout<<"\n\n\n"<<"           "<<"Please insert the title of the book"<<endl;
+string searchName,temp_title;
+
+getline (cin, searchName);
+
+
+ transform (searchName.begin(),searchName.end(),searchName.begin(),:: tolower);
+
+/*
+        for (int i=0;i<=20;i++){
+    char x[20];
+   for(int j=0; j<sizeof(book[i].title);j++)
+    x[j]=book[i].title[j];
+if (strcmp(searchName,x)==0){ */
+
+for (int i=0;i<20;i++){
+    temp_title=book[i].title;
+
+
+transform (temp_title.begin(),temp_title.end(),temp_title.begin(),:: tolower);
+
+        if (temp_title==searchName){
+
+      Num_search[counter]=i;
+      counter++;
+
+   /* for (int j=0;j<20;j++){
+        if (Num_search[j]==NULL){
+            Num_search[j]=i;
+
+             break;
+
+            }
+        }*/
+    }
+}
+
+   display (Num_search,counter);
+}
+case 'b':{cin.ignore();
+system ("cls");
+cout <<"\n\n\n"<<"           "<<"Please insert the Author of the book"<<endl;
+char searchName[20];
+gets(searchName);
+for (int i=1;i<=20;i++){
+        for (int i=0;i<=20;i++){
+    char x[20];
+   for(int j=0; j<sizeof(book[i].author);j++)
+    x[j]=book[i].author[j];
+if ((strcmp(x,searchName))==0){
+        {
+    counter++;
+    for (int j=0;j<20;j++){
+        if (Num_search[j]==0){
+            Num_search[j]=i;
+             break;
+                }
+            }
+
+        }
+    }
+}
+
+display (Num_search,counter);
+}
+case 'c':{cin.ignore();
+system ("cls");
+cout <<"\n\n\n"<<"           "<<"Please insert the ISBN of the book"<<endl;
+int isbn;
+cin >>isbn;
+for (int i=0;i<=20;i++){
+if (isbn=book[i].isbn==0){
+{
+    counter++;
+    for (int j=0;j<20;j++){
+        if (Num_search[j]==0){
+            Num_search[j]=i;
+             break;
+                }
+            }
+
+        }
+    }
+
+}
+    display (Num_search,counter);
+}
+}
+}
+}
+void listbook (char a){
+switch(a){
+case 'd': list_by_catagory();
+    break;
+case 'e': most_read_book();
+    break;
+case 'f': by_rating();
+    break;
+case 'g': recent_books();
+    break;
+case 'h': all_books();
+}
+}
+void list_by_catagory(){
+       cout <<"\n\n\n\n"<<endl;
+        cout << setw(15)<< char(176);      for(int i=0; i<90; i++){cout << char(205);} cout << char(176)<< endl;
+        cout << setw(15)<< char(176)<<"choose category "<<endl;
+        cout << setw(20)<< char(176)<<"a, Fiction "<<endl;
+        cout << setw(20)<< char(176)<<"b, Science "<<endl;
+        cout << setw(20)<< char(176)<<"c, Self help"<<endl;
+        cout << setw(20)<< char(176)<<"d, Art"<<endl;
+        cout << setw(20)<< char(176)<<"e, Religious"<<endl;
+        cout << setw(20)<< char(176)<<"f, Social science "<<endl;
+        cout << setw(20)<< char(176)<<"g, Amhaaric"<<endl;
+        cout << setw(20)<< char(176)<<"h, Research book"<<endl;
+int counter=0;
+char choice;
+cin >>choice;
+int Num_search[20];
+switch (choice){
+case 'a':{for (int i=0;i<=20;i++){
+
+        for (int i=0;i<=20;i++){
+    char x[20];
+   for(int j=0; j<sizeof(book[i].author);j++)
+    x[j]=book[i].author[j];
+if (((book[i].catagory=="fiction"))==0 ){
+        {
+    counter++;
+    for (int j=0;j<20;j++){
+        if (Num_search[j]==0){
+            Num_search[j]=i;
+             break;
+                }
+            }
+
+        }
+    }
+}
+
+display (Num_search,counter);
+}
+
+case 'b':{ for (int i=1;i<=20;i++){
+    if ((book[i].catagory=="science")){
+      {
+    counter++;
+    for (int j=0;j<20;j++){
+        if (Num_search[j]==0){
+            Num_search[j]=i;
+             break;
+                }
+            }
+
+        }
+
+    }
+}
+display (Num_search,counter);
+}
+case 'c':{for (int i=1;i<=20;i++){
+    if ((book[i].catagory=="art")){
+
+    {
+    counter++;
+    for (int j=0;j<20;j++){
+        if (Num_search[j]==0){
+            Num_search[j]=i;
+             break;
+                }
+            }
+
+        }
+    }
+}
+display (Num_search,counter);
+}
+
+case 'd': {for (int i=0;i<20;i++){
+    if ((book[i].catagory=="self help")){
+
+       {
+    counter++;
+    for (int j=0;j<20;j++){
+        if (Num_search[j]==0){
+            Num_search[j]=i;
+             break;
+                    }
+                }
+
+        }
+    }
+}
+display (Num_search,counter);
 
 }
 
+case 'e': {for (int i=0;i<20;i++){
+    if ((book[i].catagory=="Religous")){
+
+       {
+    counter++;
+    for (int j=0;j<20;j++){
+        if (Num_search[j]==0){
+            Num_search[j]=i;
+             break;
+                    }
+                }
+
+        }
+    }
+}
+display (Num_search,counter);
+
+}
+case 'f': {for (int i=0;i<20;i++){
+    if ((book[i].catagory=="Social science")){
+
+       {
+    counter++;
+    for (int j=0;j<20;j++){
+        if (Num_search[j]==0){
+            Num_search[j]=i;
+             break;
+                    }
+                }
+
+        }
+    }
+}
+display (Num_search,counter);
+
+}
+
+case 'g': {for (int i=0;i<20;i++){
+    if ((book[i].catagory=="Amharic")){
+
+       {
+    counter++;
+    for (int j=0;j<20;j++){
+        if (Num_search[j]==0){
+            Num_search[j]=i;
+             break;
+                    }
+                }
+
+        }
+    }
+}
+display (Num_search,counter);
+
+}
+
+case 'i': {for (int i=0;i<20;i++){
+    if ((book[i].catagory=="Research books")){
+
+       {
+    counter++;
+    for (int j=0;j<20;j++){
+        if (Num_search[j]==0){
+            Num_search[j]=i;
+             break;
+                    }
+                }
+
+        }
+    }
+}
+display (Num_search,counter);
+
+            }
+        }
+    }
+}
+
+void most_read_book(){
+    int Num_search[20],counter=0;
+    for (int i=0;i<20;i++){
+    if (book[i].read_freq>=2){
+            counter++;
+    for (int j=0;j<20;j++){
+        if (Num_search[j]==0){
+            Num_search[j]=i;
+             break;
+                    }
+                }
+    }
+    display (Num_search,counter);
+
+}
+}
+void by_rating(){
+   int Num_search[20],counter=0;
+        while (counter>=0){
+            for (int j=0;j<20;j++){
+        if (book[j].rate>4){
+            Num_search[counter]=j;
+            counter++;
+        }
+
+    }
+}
+display (Num_search,counter);
+}
+
+void recent_books(){
+    int Num_search[20],counter=0;
+        while (counter>=0){
+            for (int j=0;j<20;j++){
+        if (book[j].date_of_pub.yy>2020){
+            Num_search[counter]=j;
+            counter++;
+        }
+
+    }
+}
+display (Num_search,counter);
+}
+
+void all_books(){
+    int Num_search[20];
+    for (int i=0;i<20;i++){
+        Num_search[i]=i;
+    }
+    display (Num_search,sizeof(book));
+
+}
+void admin_option (){
+}
 
 
